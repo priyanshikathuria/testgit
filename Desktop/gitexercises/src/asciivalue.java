a845432ca3cfa12d3b090aa0511085f01d03dc38
PRIYANSHI KATHURIA <46554049+priyanshikathuria@users.noreply.github.com>
2019-01-16 12:48:02 +0530

    Add files via upload

---
+++ Desktop/gitexercises/src/asciivalue.java
@@ -0,0 +1,11 @@
+class asciivalue
+{
+	public static void main( String args[] )
+	{
+	char ch='a';
+	int ascii = ch;
+	int castascii = ch;
+	System.out.print("the ascii value of" +ch+ "is" +ascii);
+	System.out.print("the ascii value of" +ch+ "is" +castascii);
+	}
+}
