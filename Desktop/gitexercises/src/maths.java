65882eabf8650948c5d5c3ecf104b98e7420bb47
PRIYANSHI KATHURIA <46554049+priyanshikathuria@users.noreply.github.com>
2019-01-16 12:13:38 +0530

    Add files via upload

---
+++ Desktop/gitexercises/src/maths.java
@@ -0,0 +1,11 @@
+class maths{
+	public static void main(String args[]) {
+	int x=2;
+	int y=4;
+	int add=x+y;
+	int sub=x-y;
+	int mul=x*y;
+	int div=x/y;
+	System.out.println("Addition=" +add + "Subraction=" +sub + "Multiplication=" +mul + "Division=" +div );
+	}
+}
