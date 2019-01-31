730c4e74a6bb8d3052e4893695c52ffd7dcb0b86
PRIYANSHI KATHURIA <46554049+priyanshikathuria@users.noreply.github.com>
2019-01-31 10:58:04 +0530

    Add files via upload

---
+++ Desktop/gitexercises/src/PACKAGE/src/Abc.java
@@ -0,0 +1,8 @@
+package p1.p2;
+class disp1
+{
+	void display()
+	{
+	System.out.println("Hello");
+	}
+}
---
+++ Desktop/gitexercises/src/PACKAGE/src/Demo.java
@@ -0,0 +1,8 @@
+import p1.p2.Abc;
+class Demo
+{
+	public static void main(String args[])
+	{
+	
+	}
+}
